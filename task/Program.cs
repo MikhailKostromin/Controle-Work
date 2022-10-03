c2e2092cce9e5d11f47f3ecc53e1cda20b8c672a
Mikhail <kostrominmi@mail.ru>
2022-10-03 23:50:45 +0300

    Посчитали количество строк меньше n символов

--- task/Program.cs
+++ task/Program.cs
@@ -13,3 +13,18 @@ string[] FillArray()   // Заполняем массив
     return Console.ReadLine().Split(" ");
 }
 Console.WriteLine($"[{string.Join(", ", text)}]");
+
+Console.WriteLine("Введите максимальную длину строки которую оставим в массиве");
+int n = Convert.ToInt32(Console.ReadLine());
+int CountLess(string[] input, int n) // Считаем строки меньше трех символов
+{
+    int count = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+    return count;
+}
